daniellyvitorio/senai_coffee_Mysql_Spring

Merge remote-tracking branch 'github/master'

src/main/java/br/com/senai/controllers/AgendamentoController.java
@@ -1,0 +1,62 @@
+package br.com.senai.controllers;
+
+import br.com.senai.models.Agendamento;
+import br.com.senai.repositories.AgendamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/agendamento")
+public class AgendamentoController {
+    @Autowired
+    AgendamentoRepository agendamentoRepository;
+
+    @GetMapping(value = "/all",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Agendamento> getAllAgendamento(){
+        return agendamentoRepository.findAll();
+    }
+
+    //metodo para salvar no banco de dados
+    @PostMapping(value="/createAgendamento",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Agendamento createAgendamento(@RequestBody Agendamento agendamento){
+        //Cria um novo objeto Product
+        Agendamento newAgendamento = new Agendamento();
+        //Seta as propriedades do Product
+        newAgendamento.setName(agendamento.getName());
+        newAgendamento.setPrice(agendamento.getPrice());
+        //Chama o método save para salvar o objeto no banco de dados
+        return agendamentoRepository.save(newAgendamento);
+    }
+
+    //atualizar cafe
+    @PutMapping(value="/updateAgendamento",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Agendamento updateAgendamento(@RequestBody Agendamento agendamento){
+        Agendamento getAgendamento = agendamentoRepository.findById(agendamento.getId()).orElseThrow();
+        Agendamento updateAgendamento = new Agendamento();
+
+        updateAgendamento.setId(agendamento.getId());
+        updateAgendamento.setName(agendamento.getName());
+        updateAgendamento.setPrice(agendamento.getPrice());
+
+        return  agendamentoRepository.save(updateAgendamento);
+    }
+    // Metodo deletar product
+    @DeleteMapping(value="/deleteServico/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    //@PathVariable pega um valor passado junto a barra de endereço
+    public Agendamento deleteAgendamento(@PathVariable Long id){
+        //Verifica se existe o café no bando de dados procurando id
+        Agendamento getAgendamento = agendamentoRepository.findById(id).orElseThrow();
+        //chamamos p método .delete e passamos o café a ser deletado
+        agendamentoRepository.delete(getAgendamento);
+        return  getAgendamento;
+    }
+}

src/main/java/br/com/senai/controllers/ManutencaoControllers.java
@@ -1,0 +1,67 @@
+package br.com.senai.controllers;
+
+
+import br.com.senai.models.Manutencao;
+import br.com.senai.repositories.ManutencaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class ManutencaoControllers {
+
+    @RestController
+    @RequestMapping("/manutencao")
+    public class ManutencaoController {
+        @Autowired
+        ManutencaoRepository ManutencaoRepository;
+
+        @GetMapping(value = "/all",
+                produces = MediaType.APPLICATION_JSON_VALUE)
+        public List<Manutencao> getAllManutencao(){
+            return ManutencaoRepository.findAll();
+        }
+
+        //metodo para salvar no banco de dados
+        @PostMapping(value="/createManutencao",
+                produces = MediaType.APPLICATION_JSON_VALUE,
+                consumes = MediaType.APPLICATION_JSON_VALUE)
+        public Manutencao createManutencao(@RequestBody Manutencao manutencao){
+            //Cria um novo objeto Product
+            Manutencao newManutencao = new Manutencao();
+            //Seta as propriedades do Product
+            newManutencao.setName(manutencao.getName());
+            newManutencao.setPrice(manutencao.getPrice());
+            //Chama o método save para salvar o objeto no banco de dados
+            return ManutencaoRepository.save(newManutencao);
+        }
+
+        //atualizar cafe
+        @PutMapping(value="/updateManutencao",
+                produces = MediaType.APPLICATION_JSON_VALUE,
+                consumes = MediaType.APPLICATION_JSON_VALUE)
+        public Manutencao updateManutencao(@RequestBody Manutencao manutencao){
+            Manutencao getManutencao = (Manutencao) ManutencaoRepository.findById(manutencao.getId()).orElseThrow();
+            Manutencao updateManutencao = new Manutencao();
+
+            updateManutencao.setId(manutencao.getId());
+            updateManutencao.setName(manutencao.getName());
+            updateManutencao.setPrice(manutencao.getPrice());
+
+            return  ManutencaoRepository.save(updateManutencao);
+        }
+        // Metodo deletar product
+        @DeleteMapping(value="/deleteManutencao/{id}",
+                produces = MediaType.APPLICATION_JSON_VALUE)
+        //@PathVariable pega um valor passado junto a barra de endereço
+        public Object deleteManutencao(@PathVariable Long id){
+            //Verifica se existe o café no bando de dados procurando id
+            Object getManutencao = ManutencaoRepository.findById(id).orElseThrow();
+            //chamamos p método .delete e passamos o café a ser deletado
+            ManutencaoRepository.delet(getManutencao);
+            return getManutencao;
+        }
+    }
+
+}

daniellyvitorio/salaobeleza_back

src/main/java/br/com/senai/controllers/AgendamentoController.java
@@ -1,0 +1,68 @@
+package br.com.senai.controllers;
+
+import br.com.senai.models.Agendamento;
+import br.com.senai.repositories.AgendamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/agendamento")
+@CrossOrigin(origins = "http://localhost:5173")
+public class AgendamentoController {
+    @Autowired
+    AgendamentoRepository agendamentoRepository;
+
+    @GetMapping(value = "/all",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Agendamento> getAllAgendamento(){
+        return agendamentoRepository.findAll();
+    }
+//       /agendamento/createAgendamento
+    //metodo para salvar no banco de dados
+    @PostMapping(value="/createAgendamento",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Agendamento createAgendamento(@RequestBody Agendamento agendamento){
+        //Cria um novo objeto Product
+        System.out.println(agendamento);
+        Agendamento newAgendamento = new Agendamento();
+        //Seta as propriedades do Product
+        newAgendamento.setServico(agendamento.getServico());
+        newAgendamento.setDescricao(agendamento.getDescricao());
+        newAgendamento.setData_agendamento(agendamento.getData_agendamento());
+//        newAgendamento.setHora(agendamento.getHora());
+        //Chama o método save para salvar o objeto no banco de dados
+        return agendamentoRepository.save(newAgendamento);
+    }
+
+    //atualizar cafe
+    @PutMapping(value="/updateAgendamento",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Agendamento updateAgendamento(@RequestBody Agendamento agendamento){
+        Agendamento getAgendamento = agendamentoRepository.findById(agendamento.getId()).orElseThrow();
+        Agendamento updateAgendamento = new Agendamento();
+
+        updateAgendamento.setId(agendamento.getId());
+        updateAgendamento.setServico(agendamento.getServico());
+        updateAgendamento.setDescricao(agendamento.getDescricao());
+        updateAgendamento.setData_agendamento(agendamento.getData_agendamento());
+//        updateAgendamento.setHora(agendamento.getHora());
+
+        return  agendamentoRepository.save(updateAgendamento);
+    }
+    // Metodo deletar product
+    @DeleteMapping(value="/deleteAgendamento/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    //@PathVariable pega um valor passado junto a barra de endereço
+    public Agendamento deleteAgendamento(@PathVariable Long id){
+        //Verifica se existe o café no bando de dados procurando id
+        Agendamento getAgendamento = agendamentoRepository.findById(id).orElseThrow();
+        //chamamos p método .delete e passamos o café a ser deletado
+        agendamentoRepository.delete(getAgendamento);
+        return  getAgendamento;
+    }
+}

src/main/java/br/com/senai/controllers/ManutencaoControllers.java
@@ -1,0 +1,63 @@
+package br.com.senai.controllers;
+
+
+import br.com.senai.models.Manutencao;
+import br.com.senai.repositories.ManutencaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class ManutencaoControllers {
+
+    @RestController
+    @RequestMapping("/manutencao")
+    public class ManutencaoController {
+        @Autowired
+        ManutencaoRepository manutencaoRepository;
+
+        @GetMapping(value = "/all",
+                produces = MediaType.APPLICATION_JSON_VALUE)
+        public List<Manutencao> getAllManutencao(){
+            return manutencaoRepository.findAll();
+        }
+
+        //metodo para salvar no banco de dados
+        @PostMapping(value="/createManutencao",
+                produces = MediaType.APPLICATION_JSON_VALUE,
+                consumes = MediaType.APPLICATION_JSON_VALUE)
+        public Manutencao createManutencao(@RequestBody Manutencao manutencao){
+            //Cria um novo objeto Product
+            Manutencao newManutencao = new Manutencao();
+            //Seta as propriedades do Product
+            //Chama o método save para salvar o objeto no banco de dados
+            return manutencaoRepository.save(newManutencao);
+        }
+
+        //atualizar cafe
+        @PutMapping(value="/updateManutencao",
+                produces = MediaType.APPLICATION_JSON_VALUE,
+                consumes = MediaType.APPLICATION_JSON_VALUE)
+        public Manutencao updateManutencao(@RequestBody Manutencao manutencao){
+            Manutencao getManutencao = (Manutencao) manutencaoRepository.findById(manutencao.getId()).orElseThrow();
+            Manutencao updateManutencao = new Manutencao();
+
+            updateManutencao.setId(manutencao.getId());
+
+            return  manutencaoRepository.save(updateManutencao);
+        }
+        // Metodo deletar product
+        @DeleteMapping(value="/deleteManutencao/{id}",
+                produces = MediaType.APPLICATION_JSON_VALUE)
+        //@PathVariable pega um valor passado junto a barra de endereço
+        public Manutencao deleteManutencao(@PathVariable Long id){
+            //Verifica se existe o café no bando de dados procurando id
+            Manutencao getManutencao = manutencaoRepository.findById(id).orElseThrow();
+            //chamamos p método .delete e passamos o café a ser deletado
+            manutencaoRepository.delete(getManutencao);
+            return getManutencao;
+        }
+    }
+
+}